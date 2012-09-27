29474838d9377f2a016055866166e5c70cdf344d
pveber <philippe.veber@gmail.com>
2012-09-27 10:37:57 +0200

    macs: update to 1.4.2

--- macs/build.sh
+++ macs/build.sh
@@ -1,4 +1,4 @@
-URL=http://macs:chipseq@liulab.dfci.harvard.edu/MACS/src/MACS-1.4.1.tar.gz
+URL=https://github.com/downloads/taoliu/MACS/MACS-1.4.2-1.tar.gz
 ARCHIVE=`basename ${URL}`
 PACKAGE=macs
 
@@ -7,7 +7,7 @@ install() {
     msg Fetch ${URL}
     wget ${URL} || die "failed to fetch ${PACKAGE}"
     tar xvfz ${ARCHIVE}
-    cd ${ARCHIVE%\.tar.gz}
+    cd ${ARCHIVE%-1\.tar.gz}
     python setup.py install --prefix ${PREFIX} || die "failed to install ${PACKAGE}"
 }
 
