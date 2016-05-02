1781e7e79f0de3ec7f4925ef326eafa79ab73348
pveber <philippe.veber@gmail.com>
2016-05-02 19:24:32 +0200

    added fastool 0.1.4

---
+++ fastool/0.1.4/fastool.0.1.4.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## fastool -- installation script for fastool
+## 
+## Usage:
+##   fastool PREFIX
+##
+## if it exists, PREFIX should be a directory
+## Installation is performed in PREFIX/bin and PREFIX/src
+
+PKG=fastool
+VERSION=0.1.4
+
+set -e
+
+die() {
+    ECODE=$?
+    echo -e "$1 (\#${ECODE})"
+    exit ${ECODE}
+}
+
+if [ ! -n "$1" ]
+then
+    grep "^##" $0
+    exit 1
+fi
+
+PREFIX=$1
+if [ -a $PREFIX ] && [ ! -d $PREFIX ]
+then
+    echo "Destination $PREFIX exists and isn't a directory."
+    exit 1
+fi
+
+# make PREFIX path absolute
+PREFIX=`readlink -f $PREFIX`/$PKG/$VERSION
+
+mkdir -p $PREFIX
+cd $PREFIX
+wget https://github.com/fstrozzi/Fastool/archive/0.1.4.tar.gz
+tar xvfz 0.1.4.tar.gz
+rm 0.1.4.tar.gz
+cd Fastool-0.1.4
+make
+
+mkdir -p $PREFIX/bin
+cp fastool $PREFIX/bin
+
+cd ..
+rm -rf Fastool-0.1.4
